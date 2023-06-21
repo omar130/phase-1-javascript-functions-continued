b8bc7cef54acbb845146b611e0ef6292af54a80d
Omar Richards <omarrichards01@gmail.com>
2023-06-20 22:53:00 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+
+function saturdayFun(activity = "roller-skate"){
+ return `This Saturday, I want to ${activity}!`;
+  
+}
+
+ 
+ function mondayWork(defaultActivity = "go to the office"){
+  return `This Monday, I will ${defaultActivity}.`;
+ }
+
+   
+function wrapAdjective(string = "*"){
+  
+  return function inner(defaultAdjective = "special"){
+    return `You are ${string}${defaultAdjective}${string}!`;
+  }
+}
+
+//wrapAdjective("*")("*a hard worker*!"); //=> "You are %a dedicated programmer%!
